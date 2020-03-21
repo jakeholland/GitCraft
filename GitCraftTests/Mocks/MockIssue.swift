834dabff8473d9e4bb95a8abfadf4809ac708394
Jake Holland <jakeholland@me.com>
2020-03-21 13:13:37 -0500

    Update constraints

--- GitCraftTests/Mocks/MockIssue.swift
+++ GitCraftTests/Mocks/MockIssue.swift
@@ -10,24 +10,28 @@ import Foundation
 import GitHubService
 
 struct MockIssue: Issue {
+    
     let id: Int
     let number: Int
     let title: String
     let body: String
     let date: Date
     let lastUpdated: Date
+    let isClosed: Bool
     
     init(id: Int = -1,
          number: Int = 1,
          title: String = "Stuff is broken",
          body: String = "",
          date: Date = .distantPast,
-         lastUpdated: Date = Date.distantPast.addingTimeInterval(3600)) {
+         lastUpdated: Date = Date.distantPast.addingTimeInterval(3600),
+         isClosed: Bool = false) {
         self.id = id
         self.number = number
         self.title = title
         self.body = body
         self.date = date
         self.lastUpdated = lastUpdated
+        self.isClosed = isClosed
     }
 }
